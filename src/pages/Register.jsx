2ea7b5e2a40995c10251d70bd0aa11dcca1f84de
Shashmit kumar <shaashmit@gmail.com>
2023-08-24 22:40:59 +0530

    Storage, Register and db is working

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,9 +5,11 @@ import { auth, storage, db } from "../firebase";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
@@ -35,6 +37,8 @@ const Register = () => {
         email,
         photoURL: downloadURL,
       });
+      await setDoc(doc(db, "userChats", user.uid), {});
+      navigate("/");
     } catch (err) {
       console.log(err);
       setErr(true);
