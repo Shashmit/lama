3b90c034d8d2cb32daf44b025581d78afb7ea30c
Shashmit kumar <shaashmit@gmail.com>
2023-08-30 00:41:19 +0530

    functional search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,54 @@
-import { TextField } from '@mui/material'
-import React from 'react'
-
+import { React, useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 const Search = () => {
-  return (
-    <div className='search'>  
-    <div className="searchForm">
-      <input type="text" placeholder='Search'/>
-    </div>
-    <div className="userChat">
-      <img src="https://images.unsplash.com/photo-1692283747616-b2209a722203?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60" alt="" />
-      <div className="userChatInfo">
-        <span>Jane</span>
-      </div>
-    </div>
-    </div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
 
-export default Search
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+  const handleClick = () => {
+    
+  }
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input
+          type="text"
+          placeholder="Find a User"
+          onKeyDown={handleKey}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+      </div>
+      {err && <span>Something went wrong</span>}
+      {user && (
+        <div className="userChat" onClick={handleClick}>
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
