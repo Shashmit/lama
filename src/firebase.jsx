12a109f10704f759575535a4ee01880c73d97487
Shashmit kumar <shaashmit@gmail.com>
2023-09-05 02:28:35 +0530

    Deleted the firebase system file

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,14 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCecqm4yD7P5BOrjG3Tj6bxRL3If46kFeM",
-  authDomain: "lama-3931c.firebaseapp.com",
-  projectId: "lama-3931c",
-  storageBucket: "lama-3931c.appspot.com",
-  messagingSenderId: "146473074687",
-  appId: "1:146473074687:web:66050e0ef7dfcc1e5df1ca",
-};
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
